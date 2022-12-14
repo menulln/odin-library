b6f6f65fcd197434d308adad93d6ea06281e3997
menulln <39300523+menulln@users.noreply.github.com>
2022-12-15 00:12:47 +0100

    Create array for book objects and function addToLibrary

--- library.js
+++ library.js
@@ -1,6 +1,16 @@
+const books = [];
+
 function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read
+}
+
+function addToLibrary(title, author, pages, read) {
+    const temp = Object.create(Book.prototype, { title: { value: title, enumerable: true },
+                                       author: { value: author, enumerable: true },
+                                       pages: { value: pages, enumerable: true },
+                                       read: { value: read, enumerable: true } });
+    books.push(temp);
 }
