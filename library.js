bae9b5cb9c0e93a0ca86f208d1957202984f3a78
menulln <39300523+menulln@users.noreply.github.com>
2022-12-15 20:12:45 +0100

    Attach submit button event listener and get book data

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 const table = document.querySelector('tbody');
 const buttonAdd = document.querySelector('.button-add');
+const buttonSubmit = document.querySelector('#submit');
 const books = [];
 
 function Book(title, author, pages, read) {
@@ -36,4 +37,11 @@ buttonAdd.addEventListener('click', (e) => {
     modalClose.addEventListener('click', (e) => {
         modal.style.cssText = 'visibility: hidden;';
     });
+});
+
+buttonSubmit.addEventListener('click', (e) => {
+    const title = (document.querySelector('#title').value === '') ? 'Unknown Title' : document.querySelector('#title').value;
+    const author = (document.querySelector('#author').value === '') ? 'Unknown Author' : document.querySelector('#author').value;
+    const pages = (document.querySelector('#pages').value === '') ? 'Unknown Pages' : document.querySelector('#pages').value;
+    const read = (document.querySelector('input[name="read"]:checked').value === null) ? false : document.querySelector('input[name="read"]:checked').value;
 });
