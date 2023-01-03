7001f145a3bfb699546cf5684c2d41529a7541a1
menulln <39300523+menulln@users.noreply.github.com>
2023-01-03 13:56:08 +0100

    Refactor to use class

--- library.js
+++ library.js
@@ -3,18 +3,17 @@ const buttonAdd = document.querySelector('.button-add');
 const buttonSubmit = document.querySelector('#submit');
 let books = [];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addToLibrary(title, author, pages, read) {
-    const temp = Object.create(Book.prototype, { title: { value: title, enumerable: true },
-                                       author: { value: author, enumerable: true },
-                                       pages: { value: pages, enumerable: true },
-                                       read: { value: read, enumerable: true } });
+    const temp = new Book(title, author, pages, read);
     books.push(temp);
 }
 
